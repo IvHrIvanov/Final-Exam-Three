bc27c7942e00c3751a8ef03e65e06e73dcc7d87a
Ivan Hristoslavov <hristoslavov.ivanov@gmail.com>
2020-12-13 01:38:13 +0200

    Add project files.

---
+++ 01. World Tour/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace _01._World_Tour
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string stops = Console.ReadLine();
+
+            string[] commands = Console.ReadLine().Split(":");
+
+            while (commands[0] != "Travel")
+            {
+                string curCommand = commands[0];
+
+                if (curCommand == "Add Stop")
+                {
+                    int index = int.Parse(commands[1]);
+                    string secondStop = commands[2];
+                    if (index >= 0 && index < stops.Length)
+                    {
+                        stops = stops.Insert(index, secondStop);               
+                    }
+                }
+                else if (curCommand == "Remove Stop")
+                {
+
+                    int startIndex = int.Parse(commands[1]);
+                    int endIndex = int.Parse(commands[2]);
+                    if (startIndex >= 0 && startIndex < stops.Length && endIndex >= 0 && endIndex < stops.Length)
+                    {
+                        stops = stops.Remove(startIndex, endIndex - startIndex + 1);                       
+                    }
+
+                }
+                else if (curCommand == "Switch")
+                {
+                    string oldStop = commands[1];
+                    string newStop = commands[2];
+                    if (stops.Contains(oldStop))
+                    {
+                        stops = stops.Replace(oldStop, newStop);
+                        
+                    }
+                }
+                Console.WriteLine(stops);
+                commands = Console.ReadLine().Split(":");
+            }
+            Console.WriteLine($"Ready for world tour! Planned stops: {stops}");
+        }
+    }
+}
